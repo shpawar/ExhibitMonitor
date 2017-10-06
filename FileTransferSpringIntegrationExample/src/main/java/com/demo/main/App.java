36e3d1e6fb51d3066db9385a5e668000c455a21d
shpawar <shpawar@DIN56002187.corp.capgemini.com>
2017-10-06 18:00:24 +0530

    Initial Commit

---
+++ FileTransferSpringIntegrationExample/src/main/java/com/demo/main/App.java
@@ -0,0 +1,28 @@
+package com.demo.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class App 
+{
+	    public static void main(String... args) {
+
+	        ApplicationContext context =
+	                new ClassPathXmlApplicationContext("beans.xml");
+	    	
+	    	/*GenericXmlApplicationContext context = new GenericXmlApplicationContext("beans.xml");*/
+
+	       /* context.registerShutdownHook();
+
+	        SpringIntegrationUtils.displayDirectories(context);
+
+	        final Scanner scanner = new Scanner(System.in);
+
+	        while (!scanner.hasNext("q")) {
+	            //Do nothing unless user presses 'q' to quit.
+	        }
+
+	        System.exit(0);*/
+	    }
+}
